388ba98bd4a643cf6a13eb1aa8497e8b5ebc0a61
David Daniel <davedan7@gmail.com>
2017-03-19 13:28:38 -0600

    Communicating with backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import axios from 'axios'
 import './App.css'
 
 const App = React.createClass({
+  getInitialState () {
+    return {
+      data: {}
+    }
+  },
+  componentDidMount () {
+    axios.get('http://localhost:8080')
+      .then((res) => {
+        this.setState({ data: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
   render () {
     return (
-      <h1>Ohai</h1>
+      <div>
+        <h1>Ohai</h1>
+        <pre><code>{JSON.stringify(this.state.data)}</code></pre>
+      </div>
     )
   }
 })
